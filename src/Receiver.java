6941d50be5ea46f058c541987f948cc428d2e459
Ahmet TOPAK <75997899+ahmettopak@users.noreply.github.com>
2023-05-03 20:57:29 +0300

    Add comment

--- src/Receiver.java
+++ src/Receiver.java
@@ -8,25 +8,34 @@ import java.net.InetAddress;
 
 public class Receiver {
     public static void main(String[] args) throws Exception {
+
+        // Alıcı adresi ve bağlantı noktası belirleniyor.
         InetAddress IPAddress = InetAddress.getByName("127.0.0.1");
         int port = 5000;
+
+        // Datagram soketi oluşturuluyor.
         DatagramSocket socket = new DatagramSocket(port, IPAddress);
+
+        // Ses verileri okunacak boyutta bir byte dizisi oluşturuluyor.
         byte[] buffer = new byte[1024];
+
+        // Datagram paketi oluşturuluyor.
         DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
 
-        // Ses formatı oluşturma
+        // Ses formatını belirleme: 44.1 kHz örnekleme hızı, 16 bit çözünürlük, çift kanallı
         AudioFormat format = new AudioFormat(44100, 16, 2, true, false);
 
-        // SourceDataLine oluşturma
+        // SourceDataLine oluşturma: ses verilerini çalmak için kullanılır
         DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
         SourceDataLine line = (SourceDataLine) AudioSystem.getLine(info);
         line.open(format);
         line.start();
 
+        // Sonsuz döngü içinde, veri paketi alınır ve ses verileri çalınır
         while (true) {
-            socket.receive(packet);
-            System.out.println(packet.getData());
-            line.write(packet.getData(), 0, packet.getLength());
+            socket.receive(packet);  // Veri paketini al
+            System.out.println(packet.getData()); // Gelen veriyi ekrana yazdır (opsiyonel)
+            line.write(packet.getData(), 0, packet.getLength()); // Ses verilerini çal
         }
     }
 }
--- src/Sender.java
+++ src/Sender.java
@@ -8,28 +8,48 @@ import java.net.*;
 public class Sender {
     public static void main(String[] args) throws LineUnavailableException, IOException {
 
+        // AudioFormat nesnesi oluşturuluyor. Bu, mikrofondan alınacak sesin özelliklerini tanımlar.
         AudioFormat format = new AudioFormat(44100, 16, 2, true, false);
+
+        // AudioSystem.getTargetDataLine() metodu, sistemdeki varsayılan kaynak veri hattını döndürür.
         TargetDataLine microphone = AudioSystem.getTargetDataLine(format);
+
+        // Mikrofon veri hattı açılıyor.
         microphone.open(format);
+
+        // Mikrofon veri akışı başlatılıyor.
         microphone.start();
 
+        // Ses verileri okunacak boyutta bir byte dizisi oluşturuluyor.
         byte[] audioData = new byte[4096];
+
+        // Mikrofondan ilk okuma yapılıyor.
         int bytesRead = microphone.read(audioData, 0, audioData.length);
 
-
+        // Alıcı adresi ve bağlantı noktası belirleniyor.
         InetAddress address = InetAddress.getByName("192.168.1.6");
         int port = 5000;
+
+        // Datagram soketi oluşturuluyor.
         DatagramSocket socket = new DatagramSocket();
 
+        // Veri paketi oluşturuluyor. İlk olarak, okunan ses verileri, alıcı adresi ve bağlantı noktası parametreleriyle pakete konuluyor.
         DatagramPacket packet = new DatagramPacket(audioData, bytesRead, address, port);
+
+        // Veri paketi soket aracılığıyla gönderiliyor.
         socket.send(packet);
 
+        // Sonsuz bir döngü başlatılıyor.
         while (true) {
+            // Ses verileri tekrar okunuyor.
             bytesRead = microphone.read(audioData, 0, audioData.length);
+
+            // Paketin verileri güncelleniyor.
             packet.setData(audioData);
             packet.setLength(bytesRead);
-            socket.send(packet);
 
+            // Paket soket aracılığıyla gönderiliyor.
+            socket.send(packet);
         }
     }
 }
